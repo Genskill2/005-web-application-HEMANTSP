5c75ea7cf6a10b28d1a6981edefa1f46a5ac5cf4
hemanth <hemanth111sankar@gmail.com>
2021-07-16 11:37:44 +0530

    Round3

--- petshop/pets.py
+++ petshop/pets.py
@@ -36,25 +36,25 @@ def dashboard():
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.id")
       else:
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.id desc")
-      elif oby[1] == request.args.get("order_by", "name"):
+    elif oby[1] == request.args.get("order_by", "name"):
       order = request.args.get("order", "asc")
       if order == "asc":
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.name")
       else:
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.name desc")
-      elif oby[2] == request.args.get("order_by", "bought"):
+    elif oby[2] == request.args.get("order_by", "bought"):
       order = request.args.get("order", "asc")
       if order == "asc":
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.bought")
       else:
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.bought desc")
-      elif oby[3] == request.args.get("order_by", "sold"):
+    elif oby[3] == request.args.get("order_by", "sold"):
       order = request.args.get("order", "asc")
       if order == "asc":
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.sold")
       else:
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.sold desc")
-      elif oby[4] == request.args.get("order_by", "species"):
+    elif oby[4] == request.args.get("order_by", "species"):
       order = request.args.get("order", "asc")
       if order == "asc":
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by s.name")
